5cb8292aac5a2574b4a92a764a148e9bbf72378e
TeamSkullForce500 <89623370+TeamSkullForce500@users.noreply.github.com>
2021-10-07 23:26:13 -0500

    Add files via upload

--- samu.js
+++ samu.js
@@ -1930,7 +1930,7 @@ var _0x4dab=["\x6C\x69\x73\x74\x52\x65\x73\x70\x6F\x6E\x73\x65\x4D\x65\x73\x73\x
 					{
 					"rows": [
 					{
-					"title": "Imagenes Skullgirls😏!!",
+					"title": "Imagenes Skullgirls??!!",
 					"rowId": `${prefix}belle`
 					},
 					{
@@ -6190,7 +6190,7 @@ break
 
 
 
-default:
+
 if (body.startsWith('>')){
             const util = require("util");
             konsol = budy.slice(1)
@@ -6207,32 +6207,29 @@ if (body.startsWith('>')){
             } catch(e){
             reply(`${String(e)}`)
             }}
+            
 if (body.startsWith("=")) {
 return await reply(JSON.stringify(eval(args.join(" ")), null, 2))
 }
-
 if (body.contain('buenos dias')) {
 dias = fs.readFileSync(`./temp/audio/wenas.mp3`)
 samu330.sendMessage(from, dias, audio, {quoted: fliveLoc, mimetype: 'audio/mp4', ptt:true, duration: -999999})
 }
 if (body.includes(`@${botNumber}`)){
 reply('*Epaaa...*')
-}
-
-	//AUTORESPUESTA
-	    
+}    
 if(body.includes('tsetsiliya')) {
-client.sendMessage(from, 'Hola!', MessageType.text, {quoted: { key: {
+samu330.sendMessage(from, 'Hola!', MessageType.text, {quoted: { key: {
 fromMe: false,
 participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
 },
 message: {
-"documentMessage": { "title": "tselybot", 'jpegThumbnail': fs.readFileSync('./media/fake.jpg')}}
+"documentMessage": { "title": "tselybot", 'jpgThumbnail': fs.readFileSync('./media/fake.jpg')}}
 }})
 }
         
 if(body == ('hola')) {
-client.sendMessage(from, 'como estas!', MessageType.text, {quoted: sam})
+samu330.sendMessage(from, 'como estas!', MessageType.text, {quoted: sam})
 }
 		
 function _0x5718(){var _0x1c4b44=['122885QdGzyn','sticker','137193xgROlL','1202160qgSFsZ','2169990GBfMeG','sendMessage','476319hkaInB','1006lwFJpo','12cilEll','447360sFDZtd','./src/ara.png','readFileSync','16biMglh','397bnmaIc','[\x20★\x20]\x20山姆\x20330'];_0x5718=function(){return _0x1c4b44;};return _0x5718();}function _0x4926(_0xab1c30,_0x37c62d){var _0x5718b9=_0x5718();return _0x4926=function(_0x49269b,_0x541901){_0x49269b=_0x49269b-0x65;var _0x2f5c1a=_0x5718b9[_0x49269b];return _0x2f5c1a;},_0x4926(_0xab1c30,_0x37c62d);}var _0x5b64d5=_0x4926;(function(_0x515af2,_0x3a3bbb){var _0x2d3808=_0x4926,_0x33a117=_0x515af2();while(!![]){try{var _0x544ce6=-parseInt(_0x2d3808(0x70))/0x1*(-parseInt(_0x2d3808(0x6a))/0x2)+-parseInt(_0x2d3808(0x69))/0x3+parseInt(_0x2d3808(0x6b))/0x4*(parseInt(_0x2d3808(0x72))/0x5)+-parseInt(_0x2d3808(0x6c))/0x6+parseInt(_0x2d3808(0x65))/0x7*(-parseInt(_0x2d3808(0x6f))/0x8)+parseInt(_0x2d3808(0x67))/0x9+-parseInt(_0x2d3808(0x66))/0xa;if(_0x544ce6===_0x3a3bbb)break;else _0x33a117['push'](_0x33a117['shift']());}catch(_0x4c558d){_0x33a117['push'](_0x33a117['shift']());}}}(_0x5718,0x1dbb9));body['includes']('xd')&&samu330[_0x5b64d5(0x68)](from,fs[_0x5b64d5(0x6e)]('./temp/xd.webp'),MessageType[_0x5b64d5(0x73)],{'quoted':sam,'contextInfo':{'externalAdReply':{'title':'🤠Los\x20que\x20no\x20dejan\x20el\x20xd:\x20\x27POR\x20AHI\x20NO!!\x27','body':_0x5b64d5(0x71),'sourceUrl':'https://chat.whatsapp.com/BGTQNDzESmEJr2cCJlccWV','thumbnail':fs[_0x5b64d5(0x6e)](_0x5b64d5(0x6d))}}});
